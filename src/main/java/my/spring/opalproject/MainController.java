435b0a6fda65c59cefc48e44155382e7e170b549
jeong-hae-rim <use_mono@naver.com>
2020-05-13 17:12:26 +0900

    MainController 메서드 추가. 페이지 3개 생성. main, about 페이지의 링크 수정.

--- src/main/java/my/spring/opalproject/MainController.java
+++ src/main/java/my/spring/opalproject/MainController.java
@@ -11,11 +11,36 @@ public class MainController {
 		
 		return  "main";
 	}
+	//메인 페이지 컨트롤러
 	
 	@RequestMapping(value="/about")
 	public String about() {
 		
 		return  "about";
 	}
+	//소개 페이지 컨트롤러 -- 클래스 분할해서 사용해도 됨
+	
+	@RequestMapping(value="/datamain")
+	public String datamain() {
+		
+		return  "datamain";
+	}
+	//질병DATA 컨트롤러 -- 클래스 분할해서 사용해도 됨
+	
+	@RequestMapping(value="/goods")
+	public String goods() {
+		
+		return  "goods";
+	}
+	//농산물구매 컨트롤러 -- 클래스 분할해서 사용해도 됨
+	
+	@RequestMapping(value="/markets")
+	public String markets() {
+		
+		return  "markets";
+	}
+	//농가별구매 컨트롤러 -- 클래스 분할해서 사용해도 됨
+	
+	
 
 }
